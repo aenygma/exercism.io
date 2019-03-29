56266d2014f0159e513cc7c15f104e89e01c9185
aenygma <uttieg@gmail.com>
2019-03-28 22:38:00 -0700

    add book_store

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,47 +1,85 @@
+class Chunk(object):
+    """
+    given a list of items, chunk them in unique groups
+    """
 
+    def __init__(self, tlist):
+        self.tlist = tlist.copy()
+
+    @property
+    def counts(self):
+        """  return a tally of counts"""
+
+        ret = []
+        for i in set(self.tlist):
+            ret.append((i, self.tlist.count(i)))
+
+        # sort by counts
+        ret.sort(key=lambda x: x[1], reverse=True)
+        return ret
+
+    def get_uniq_n(self, num=4):
+        """ return n number of uniq items """
+
+        ret = []
+        tmp = []
+        idx = 0
+
+        # iterate list, or until 4 items grabbed
+        #while (idx < len(self.counts)) and (len(tmp) < num):
+
+        while True:
+            print("> x:", idx, tmp, ret)
+
+            # done with group, next group
+            if len(tmp) >= num:
+                ret.append(tmp)
+                tmp = []
+                idx = 0
+
+            # we're done with entire list
+            if idx >= len(self.counts):
+                ret.append(tmp)
+                tmp = []
+                idx = 0
+
+            # return
+            if not self.tlist:
+                return ret
+
+            val, _ = self.counts[idx]
+            if val not in tmp:
+                idx = 0
+                tmp.append(val)
+                self.tlist.remove(val)
+            else:
+                idx += 1
+
+    def __repr__(self):
+        return repr(self.tlist)
+
+    get_groups = get_uniq_n
 
 DISCOUNT_TABLE = {1: 1.0, 2: 0.95, 3: 0.9, 4: 0.8, 5: 0.75}
-COST_TABLE = {1: 8*1.0, 2: 8*2*0.95, 3: 8*3*0.9, 4: 8*4*0.8, 5: 8*5*0.75}
 
 def cost(books):
-    """ determine max discount for given books """
+    """ return cost for a set of books """
 
-    # make a local copy before we mutate it
-    local_books = books.copy()
-    total_books = len(books)
-    book_types = list(set(books))
-    discount = DISCOUNT_TABLE.get(len(book_types), 0)
-    # cost = bookprice * (books_without_discount + books_with_discount)
-    cost = 8 * ((total_books - len(book_types)) + discount*len(book_types))
-    return cost * 100
+    num_types = len(set(books))
+    assert num_types < 6, "Discounts indeterminate"
+    return DISCOUNT_TABLE.get(num_types, 0) * 8 * len(books) * 100
 
 def calculate_total(books):
+    """ return minimum cost for all books, per the discounts """
 
     if len(books) <= 5:
         return cost(books)
-    return cost(books)
 
-def make_groups(tbooks):
+    # try 4-group chunks
+    cost4 = sum(map(cost, Chunk(books).get_groups(4)))
 
-    ret = []
-    books = tbooks.copy()
+    # try 5-group chunks
+    cost5 = sum(map(cost, Chunk(books).get_groups(5)))
 
-    while len(books) > 0:
-        bset = list(set(books))
-        [books.remove(i) for i in bset]
-        ret.append(bset)
-    return ret
-
-#
-# Note: 4 is the optimal number because
-#   - a group of 5 doesn't offer a greater discount
-#   - so, any extra over 4 would help bring other
-#       groups to a higher discount bracker
-
-def optimize(group1, group2):
-
-    if not (((len(group1) > 4) and (len(group2) < 4)) or \
-        ((len(group1) < 4) and (len(group2) > 4))):
-        # already optimized
-        return
-
+    # return whatever's smaller
+    return min(cost4, cost5)
